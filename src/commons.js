6871e81f1782a1e376a05d1982630cb8229b633b
Tino Dietel <tid@tradebyte.biz>
2019-01-25 21:26:20 +0100

    move create of platform and player to commons.js

--- src/commons.js
+++ src/commons.js
@@ -11,5 +11,39 @@ export default {
         'assets/dude.png',
         { frameWidth: 32, frameHeight: 48 }
     );
+  },
+
+  createPlatform(context) {
+    context.platforms = context.physics.add.staticGroup();
+  },
+
+  createPlayer(context) {
+    context.player = context.physics.add.sprite(100, 450, 'dude');
+
+    context.player.setBounce(0.2);
+    context.player.setCollideWorldBounds(true);
+
+    context.anims.create({
+        key: 'left',
+        frames: context.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    context.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    context.anims.create({
+        key: 'right',
+        frames: context.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    context.cursors = context.input.keyboard.createCursorKeys();
+    context.physics.add.collider(context.player, context.platforms);
   }
 }
--- src/scenes/GameScene2.js
+++ src/scenes/GameScene2.js
@@ -21,62 +21,33 @@ export default class GameScene2 extends Phaser.Scene {
    * Create game start.
    */
   create () {
+    commons.createPlatform(this);
 
-        this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'sky');
 
-        this.platforms = this.physics.add.staticGroup();
+    // this.platforms added by commons.createPlatform()
+    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    this.platforms.create(600, 400, 'ground');
+    this.platforms.create(50, 250, 'ground');
+    this.platforms.create(750, 220, 'ground');
 
-        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    commons.createPlayer(this);
+    this.score = 0;
 
-        this.platforms.create(600, 400, 'ground');
-        this.platforms.create(50, 250, 'ground');
-        this.platforms.create(750, 220, 'ground');
+    this.stars = this.physics.add.group({
+        key: 'bomb',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
 
-        this.player = this.physics.add.sprite(100, 450, 'dude');
+    this.stars.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
 
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
+    this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    this.physics.add.collider(this.stars, this.platforms);
 
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'turn',
-            frames: [ { key: 'dude', frame: 4 } ],
-            frameRate: 20
-        });
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.score = 0;
-
-        this.stars = this.physics.add.group({
-            key: 'bomb',
-            repeat: 11,
-            setXY: { x: 12, y: 0, stepX: 70 }
-        });
-
-        this.stars.children.iterate(function (child) {
-
-            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-        });
-
-        this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-        this.physics.add.collider(this.player, this.platforms);
-        this.physics.add.collider(this.stars, this.platforms);
-
-        this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+    this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
   }
 
   /**
